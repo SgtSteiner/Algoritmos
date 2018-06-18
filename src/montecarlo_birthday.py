9fbae20371d9aacb56f2cafa87ab6fae4298409f
Steiner <futitotal@gmail.com>
2018-06-18 21:13:40 +0200

    refactoring

--- src/montecarlo_birthday.py
+++ src/montecarlo_birthday.py
@@ -1,8 +1,19 @@
 import random
 import collections
+import matplotlib.pyplot as plt
 
-CYCLES = 10000
+CYCLES = 5000
 MAX_STUDENTS = 50
+hist = {}
+
+
+def draw_hist(data):
+    plt.plot(data.values())
+    plt.xlabel('Students')
+    plt.ylabel('Probability')
+    plt.axis([2, 50, 0, 1])
+    plt.show()
+
 
 for students in range(2, MAX_STUDENTS+1):
     res = 0
@@ -10,4 +21,7 @@ for students in range(2, MAX_STUDENTS+1):
         classroom = collections.Counter([random.randint(1, 365) for _ in range(students)])
         if max(classroom.values()) > 1:
             res += 1
-    print("Students: {0} - Prob: {1:.2f}".format(students, res/CYCLES))
+    hist[students] = res/CYCLES
+    print("Students: {0} - Prob: {1:.2%}".format(students, res/CYCLES))
+
+draw_hist(hist)
--- src/montecarlo_us.py
+++ src/montecarlo_us.py
@@ -4,41 +4,57 @@ import matplotlib.pyplot as plt
 from collections import Counter
 
 
-def draw_histogram(ar):
-    plt.hist(ar, bins=50, alpha=1, edgecolor='black', linewidth=1)
-    plt.axvline(np.percentile(ar, 5), color='r', linestyle='dashed', linewidth=2)
-    plt.axvline(np.percentile(ar, 95), color='r', linestyle='dashed', linewidth=2)
-    # plt.grid(True)
-    plt.show()
-    plt.clf()
+class Montecarlo:
+    def __init__(self, initial_features, initial_projects):
+        self.features = initial_features
+        self.projects = initial_projects
+        self.results = []
+        self.hist = {}
 
+    def run(self):
+        for _ in range(self.projects):
+            acum = 0
+            sprints = 0
+            while acum < self.features:
+                sprints += 1
+                acum += random.randint(1, 6)
+            self.results.append(sprints)
 
-def montecarlo(initial_us, initial_projects):
-    user_stories = initial_us
-    projects = initial_projects
-    results = []
+        self.hist = Counter(self.results)
 
-    for _ in range(projects):
-        acum = 0
-        sprints = 0
-        while acum < user_stories:
-            sprints += 1
-            acum += random.randint(1, 6)
-        results.append(sprints)
+    def print_results(self):
+        for sprints, percentage in sorted([(key, self.hist[key] / self.projects) for key in self.hist]):
+            print("{}\t{:>7.3%}".format(sprints, percentage))
 
-    hist = Counter(results)
-    for sprint, percentage in sorted([(key, hist[key]/initial_projects) for key in hist]):
-        print("{}\t{:>7.3%}".format(sprint, percentage))
+        print("Media = {0:.0f}".format(np.mean(self.results)))
+        print("85% quantile = {0:.0f}".format(np.percentile(self.results, 85)))
 
-    print("Media = {0:.0f}".format(np.mean(results)))
-    print(" 5% quantile = {0:.0f}".format(np.percentile(results, 5)))
-    print("95% quantile = {0:.0f}".format(np.percentile(results, 95)))
+    def draw_histogram(self):
+        plt.hist(self.results, bins=len(self.hist), alpha=1, edgecolor='black', linewidth=1)
+        plt.axvline(np.percentile(self.results, 85), color='r', linestyle='dashed', linewidth=2)
+        plt.xlabel("Nº sprints")
+        # plt.grid(True)
+        plt.show()
+        plt.clf()
 
-    return results
+    def draw_bar(self):
+        plt.bar(self.hist.keys(), [value/self.projects for value in self.hist.values()])
+        plt.suptitle("Método Montecarlo")
+        plt.title("Probabilidad de finalizar {0} features tras {1:,} iteraciones".format(
+            self.features, self.projects).replace(",", ".")
+        )
+        plt.ylabel("Probabilidad")
+        plt.xlabel("Nº de sprints")
+        plt.show()
+        plt.clf()
 
 
 if __name__ == '__main__':
     USER_STORIES = 30   # Number of user stories per project
-    PROJECTS = 50000   # Number of projects to be executed
+    PROJECTS = 500000   # Number of projects to be executed
 
-    draw_histogram(montecarlo(USER_STORIES, PROJECTS))
+    mc = Montecarlo(USER_STORIES, PROJECTS)
+    mc.run()
+    mc.print_results()
+    # mc.draw_histogram()
+    mc.draw_bar()
