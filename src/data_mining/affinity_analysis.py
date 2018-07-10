4305e43d80abce0c5dd62c7851164294e6aae305
Steiner <futitotal@gmail.com>
2018-07-10 23:07:59 +0200

    refactoring affinity analysis

--- src/data_mining/affinity_analysis.py
+++ src/data_mining/affinity_analysis.py
@@ -13,27 +13,34 @@ def print_rule(premise, conclusion, support, confidence, features):
 
 if __name__ == "__main__":
 
+    # Nombre de las features, por referencia
     features = ["pan", "leche", "queso", "manzanas", "plátanos"]
 
     # Cargamos el dataset con las muestras de compras realizadas de las diferentes features
     dataset_filename = "datasets\\affinity_dataset.txt"
     x = np.loadtxt(dataset_filename)
+    n_samples, n_features = x.shape
+    print("Este dataset contiene {0} muestras y {1} features".format(n_samples, n_features))
 
     valid_rules = defaultdict(int)
     invalid_rules = defaultdict(int)
     num_occurances = defaultdict(int)
 
+    # Se procesan todas las reglas posibles
     for sample in x:
         for premise in range(len(features)):
             if sample[premise] == 0:
                 continue
+            # Se registra que la premisa fue comprada en la transacción
             num_occurances[premise] += 1
             for conclusion in range(len(features)):
                 if premise == conclusion:   # se excluyen casos como "si se compra pan también se compra pan"
                     continue
                 if sample[conclusion] == 1:
+                    # Está persona también compró la conclusión
                     valid_rules[(premise, conclusion)] += 1
                 else:
+                    # Esta persona compró la premisa, pero no la conclusión
                     invalid_rules[(premise, conclusion)] += 1
 
     support = valid_rules
